f74b0e73c2f4c9212b9b5a973c9bbfeb23b6531e
John Albert Bertuldo <johnalbert.bertuldo@gcash.com>
2023-05-26 10:31:35 +0800

    Added scratch files for day 5

---
+++ day5/scratch files/Blueberry.java
@@ -0,0 +1,6 @@
+public class Blueberry implements Cake {
+    @Override
+    public void slice() {
+        System.out.println("Cake has been sliced");
+    }
+}
---
+++ day5/scratch files/TopicInterface.java
@@ -0,0 +1,11 @@
+public class TopicInterface {
+    public static void main(String[] args) {
+        Blueberry blueberry = new Blueberry();
+
+
+        // if Blueberry is a cake, slice the Blueberry
+        if(blueberry instanceof Cake){
+            blueberry.slice();
+        }
+    }
+}
